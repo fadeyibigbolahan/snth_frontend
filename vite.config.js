e52f1ccf6f8b4123cf83103754b6048e51705254
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-06-29 11:19:03 +0100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/snth_frontend/",
   plugins: [react()],
   resolve: {
     alias: {
