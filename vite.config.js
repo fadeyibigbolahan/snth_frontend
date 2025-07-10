de37d0042528e609b563018589433414fd3e8905
fadeyibi gbolahan <fadeyibi26@gmail.com>
2025-07-10 15:46:01 +0100

    added domain

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/snth_frontend/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
